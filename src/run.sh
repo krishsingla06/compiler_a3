7c7d14503eb2ca8951575e081f5c9c87018bd0ff
Shubham Kataria <148683207+goat-simulator444@users.noreply.github.com>
2025-10-18 13:14:33 +0530

    Add files via upload

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for file in ../test_cases/*; do
-    echo 'Running parser on $file'
-    ./parser $file
+rm -f test_cases/*.errors
+
+for file in test_cases/*; do
+    echo "Running parser on $file"
+    ./src/parser "$file"    
 done
+
+# rm -f newwtestcases/*.errors
+
+# for file in newwtestcases/*.cpp; do
+#     echo "Running parser on $file"
+#     ./src/parser "$file"      
+# done
