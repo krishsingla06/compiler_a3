003b50f2b6c1fdb308281fbea7be247c7b60c977
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-21 21:41:52 +0530

    multiple arguments case

---
+++ test_cases/many_arguments.c
@@ -0,0 +1,10 @@
+int foo(int y,int yy){
+
+}
+void foo(int y,int yy, int yyy , int yyyy, int yyyyy, int yyyyyy, int yyyyyyy, int yyyyyyyy, int yyyyyyyyy, int yyyyyyyyyy){ 
+    return ;
+}
+int main(){
+    int p;
+    foo(p,p,p,p,p,p,p,p,p,p);
+}
