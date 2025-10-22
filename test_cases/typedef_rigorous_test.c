bd761017e6c940f4329c6b2218a98433527ed5d0
Krish Singla <148368716+krishsingla06@users.noreply.github.com>
2025-10-22 19:07:19 +0530

    typedef + nested structs + anonymous structs

--- test_cases/typedef_rigorous_test.c
+++ test_cases/typedef_rigorous_test.c
@@ -1,6 +1,17 @@
 typedef char* string;
 typedef int array[1024];
 
+typedef struct {
+    int a;
+    float b;
+} myStruct;
+
+typedef struct {
+    int x;
+    myStruct y;
+    char z;
+} point;
+
 
 int main(){
     
@@ -9,6 +20,8 @@ int main(){
     typedef int i;
     i x;
     typedef char string;
+    myStruct ms;
+    point p;
     //string str2 = "Hello, World!"; // Type mismatch in initialization of variable 'str2': cannot convert from char* to char
     
     //arr=str;  Type Error at line 8: Cannot assign char* to int[1024]
@@ -19,8 +32,14 @@ int main(){
         typedef int i[10];
        i y;
        y[0] = 7;
+       ms.b = y[0];
+
        //y=1; // Type Error at line 22: Cannot assign int to int[10]
     }
+    p.x=1;
+    p.y.a = 5;
+    p.y.b = 3.14;
+    p.z = 'c';
     return 0;
 }
 
