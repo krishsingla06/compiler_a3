c4b3f6b03f02f2b5c2cd25f90eaaaa26eab4f82a
5h3rl0ckH0lm3s <kritik1026@gmail.com>
2025-10-24 17:14:27 +0000

    Ambiguous type warning found in References & enums

---
+++ integration_test_cases/enum.c
@@ -0,0 +1,14 @@
+// not passed yet
+
+enum Animal{
+    DOG,
+    CAT,
+    BIRD = 5,
+    FISH
+};
+
+enum Animal * func(){
+    int *a;
+    return a;
+}
+
---
+++ integration_test_cases/references.c
@@ -0,0 +1,8 @@
+// not passed yet 
+
+int func(){
+    int a = 0;
+    char c = &a;
+    c = 7;
+    return c;
+}
