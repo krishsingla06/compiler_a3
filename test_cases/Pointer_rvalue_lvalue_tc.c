16b62fb31ec8bb66d881c18591fdd950ce35073e
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-24 13:52:15 +0530

    test_case for checking lvalue and rvalue in pointers

---
+++ test_cases/Pointer_rvalue_lvalue_tc.c
@@ -0,0 +1,16 @@
+struct tt{
+    int a;
+    // int foo(int x);
+};
+
+int foo(int yy, int hh){
+
+}
+int foo(int kk){
+    return 0;
+}
+int main(){
+    int a = 0;
+    // int* ii = 0; // This is giving error as it should
+    int* iy = &a; //This is not giving error and it should not
+    }
