8c686502f28f5d2b919d028f4208c8e667c82506
5h3rl0ckH0lm3s <kritik1026@gmail.com>
2025-10-24 10:07:50 +0000

    infinite loop bug found

---
+++ integration_test_cases/infinite_loop_control_flow.c
@@ -0,0 +1,17 @@
+// not passed yet
+
+void foo(){
+    label2:
+
+    until(1){
+        goto label2;
+    }
+}
+
+// not passed yet
+void boo(){
+    int i = 0;
+    while(1){
+        continue;
+    }
+}
