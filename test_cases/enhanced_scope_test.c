f98b54cd2ebcc913d695d17117912677eec3a52c
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-27 21:59:52 +0530

    Update enhanced_scope_test.c

--- test_cases/enhanced_scope_test.c
+++ test_cases/enhanced_scope_test.c
@@ -5,10 +5,6 @@ int main() {
     // Basic declarations
     int a, b, c;              // Multiple variables of same type
     static int x;             // Static variable
-    // const int y;              // Const variable
-    // static const int z;       // Static const variable
-    
-    // With initialization
     int i = 5, j = 10, k;     // Mixed initialized and uninitialized
     
     // Usage
@@ -18,11 +14,10 @@ int main() {
     
     {                         // New scope
         int local_a;          // Shadow outer variable
-        const int local_const = 20;
+        int local_const = 20;
         static int local_static;
         
         local_a = a;          // Use outer variable
-        // local_const = 25;  // This should be an error (const assignment)
     }
     
     return 0;
