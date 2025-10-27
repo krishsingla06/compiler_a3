fa2208eb1f8b6e57dabcc36f7fbcb32ad6771492
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-27 19:26:20 +0530

    updated tc

--- integration_test_cases/pass_by_address.c
+++ integration_test_cases/pass_by_address.c
@@ -1,12 +1,10 @@
-// passed
-
 int * func(float inp){
     int a = inp;
-    return a;
+    return &a;
 }
 
 int main(){
     float var = 5.5;
-    int* res = func(&var);
+    int* res = func(var);
     return 0;
 }
