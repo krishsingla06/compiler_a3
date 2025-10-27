7c1a5fc3798289549845d7f00661be21b511a21e
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-27 21:53:07 +0530

    Clean up comments in classtc1.c
    Removed comments about implicit conversion and negative test.

--- test_cases/classtc1.c
+++ test_cases/classtc1.c
@@ -14,8 +14,5 @@ int main() {
     class Point b(3);      
     class Point c(3, 7);   
     a.print(); b.print(); c.print();
-    // The next line must NOT compile if implicit conversion is allowed.
-    // Uncomment to negative-test your compiler:
-    // takesPoint(5); // should fail due to 'explicit'
     return 0;
 }
