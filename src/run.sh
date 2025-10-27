1992dd2565522d8f17844f5afcd1aaa95c8d986d
Kritik Vijay <148383455+5h3rl0ckH0lm3s@users.noreply.github.com>
2025-10-27 20:59:16 +0530

    change in run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 rm -f ../test_cases/*.errors
 
-for file in ../test_cases/*; do
-    echo "Running parser on $file"
-    ./parser "$file"    
+for file in ../test_cases/*.c; do
+    # Check if the file actually exists (in case no .c files are found)
+    if [ -f "$file" ]; then
+        echo "Running parser on $file"
+        ./parser "$file"
+    fi
 done
-
-# rm -f newwtestcases/*.errors
-
-# for file in newwtestcases/*.cpp; do
-#     echo "Running parser on $file"
-#     ./src/parser "$file"      
-# done
-
-
