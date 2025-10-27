bdd9d75636c19aab5a74363fed91be6f577363e6
ARNAV GUPTA <148369134+arnav04g@users.noreply.github.com>
2025-10-27 21:42:32 +0530

    updated

--- integration_test_cases/references.c
+++ integration_test_cases/references.c
@@ -1,8 +1,8 @@
 // passed 
-// this is giving error and it should give an error
 int func(){
     int a = 0;
-    char c = &a;
+    int& c = a;
+    // char&d = a; // strict type required for references as per C++ standard
     c = 7;
     return c;
 
