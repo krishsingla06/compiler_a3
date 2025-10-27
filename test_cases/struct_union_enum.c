cddf8dd826e35bb07f2536902478799753590892
Shubham Kataria <148683207+goat-simulator444@users.noreply.github.com>
2025-10-27 22:10:07 +0530

    improved the struct test cases

--- test_cases/struct_union_enum.c
+++ test_cases/struct_union_enum.c
@@ -1,43 +1,59 @@
+// Test file for struct and union support
+
+// Define a struct
 struct Point {
     int x;
     int y;
 };
 
-// union Data {
-//     int i;
-//     float f;
-//     char c;
-// };
+// Define a union
+union Data {
+    int i;
+    float f;
+    char c;
+};
 
-// enum Color {
-//     RED,
-//     GREEN,
-//     BLUE
-// };
+// Define a nested struct
+struct Rectangle {
+    struct Point topLeft;
+    struct Point bottomRight;
+};
 
 int main() {
-    struct Point p;
-    // union Data d;
-    // enum Color col;
-    int sum = p.x + p.y;
-    int value = d.i;
-    float fval = d.f;
-    char ch = d.c;
-    int color_code = col;
-    float* fptr = NULL;
-    int* akad,bakad,bmbe,bo;
-    struct Point* pp = &p;
-    p.x = 10;
-    p->y = 20;
-
-    d.i = 100;
+    // Declare struct variables
+    struct Point p1;
+    struct Point p2;
+    struct Rectangle rect;
+    
+    
+    // Declare union variable
+    union Data d;
+    
+    // Declare pointer to struct
+    struct Point* ptr;
+    
+    // Access struct members with dot operator
+    p1.x = 10;
+    p1.y = 20;
+    
+    p2.x = p1.x + 5;
+    p2.y = p1.y - 3;
+    p1=p2;
+    
+    // Access union members
+    d.i = 42;
     d.f = 3.14;
-    d.c = 'A';
-
-   
-    col = GREEN;
-
-    // Use the variables in some way
-
+    
+    
+    // Access struct members with arrow operator
+    ptr->x = 100;
+    ptr->y = 200;
+    
+    // Nested struct access
+    rect.topLeft.x = 0;
+    rect.topLeft.y = 0;
+    rect.bottomRight.x = 100;
+    rect.bottomRight.y = 100;
+    
     return 0;
 }
