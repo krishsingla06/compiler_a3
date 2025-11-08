75030a2805db7a1845d441d8ccb58de0aaf58b44
Krish Singla <148368716+krishsingla06@users.noreply.github.com>
2025-11-08 22:28:55 +0530

    Refactor comments for clarity and add new note

--- imp.c
+++ imp.c
@@ -1,2 +1,4 @@
 // for now we are spilling aa the regs before call and after return
 // other approache might be freeing up all the regs, store both descriptor files, then clear regs files and after calling, take back both descriptor files and continue
+
+x++ for floats mei abhi type conversion baaki hai
